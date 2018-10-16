e8f5a207ee2d136338e0bcdc15861c9b502c8b1f
coltonteefy <coltonteefy@yahoo.com>
2018-10-16 10:51:17 -0700

    add registration

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,61 @@
+import {Component, OnInit} from '@angular/core';
+import {ApiService} from "../services/api.service";
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import{User} from "../models/user"
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  registerForm:FormGroup;
+  name:string = '';
+  email:string = '';
+  username:string = '';
+  password:string = '';
+  password2:string = '';
+  submitted = false;
+  passwordMatch = false;
+
+  constructor(private apiSevice:ApiService, private formBuilder:FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      'name': new FormControl('', [Validators.required]),
+      'email': new FormControl('', [Validators.required]),
+      'username': new FormControl('', [Validators.required]),
+      'password': new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)])),
+      'password2': new FormControl('', [Validators.required]),
+    })
+  }
+
+  registerSubmit() {
+    this.submitted = true;
+
+    this.name = this.registerForm.controls.name.value;
+    this.email = this.registerForm.controls.email.value;
+    this.username = this.registerForm.controls.username.value;
+    this.password = this.registerForm.controls.password.value;
+    this.password2 = this.registerForm.controls.password2.value;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      console.log(this.registerForm.invalid);
+      console.log(this.registerForm.controls.password);
+      console.log(this.name, this.email, this.username, this.password, this.password2);
+      return;
+    }
+
+    if (this.password != this.password2) {
+      console.log("NO MATCH");
+      return;
+    }
+
+    this.passwordMatch = true;
+    console.log(this.name, this.email, this.username, this.password, this.password2);
+    console.log(this.registerForm.value);
+    console.log(this.registerForm.invalid);
+  }
+}
