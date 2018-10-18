a55bb96c11a5c4cc36d42c8cb2e8715d4ea67027
Alaina Hammonds <ahammonds@onetrust.com>
2018-10-17 22:09:12 -0400

    Add forms component, fix top navbar dropdown menu bug

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  formsList = [
+    {
+      formName: "New Patient Form",
+      formDate: "1/1/11",
+      formLink: "BarGraph.pdf",
+    },
+    {
+      formName: "Medical Background Form",
+      formDate: "2/2/12",
+      formLink: "BarGraph.pdf",
+    },
+    {
+      formName: "Medical Release Form",
+      formDate: "3/3/13",
+      formLink: "BarGraph.pdf",
+    },
+    {
+      formName: "Change of Insurance Form",
+      formDate: "1/1/11",
+      formLink: "BarGraph.pdf",
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() {
