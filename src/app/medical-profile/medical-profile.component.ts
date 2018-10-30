6fc21e9e7d5e435c5fc2d139fbede9e32e297cd1
aseling <43179307+aseling@users.noreply.github.com>
2018-10-30 14:30:57 -0400

    Medical Profile

--- src/app/medical-profile/medical-profile.component.ts
+++ src/app/medical-profile/medical-profile.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-medical-profile',
   templateUrl: './medical-profile.component.html',
   styleUrls: ['./medical-profile.component.css']
+
 })
 export class MedicalProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+}
+
+export class patientContactInformation{
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
 }
