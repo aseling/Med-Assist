4aba8e0d93899b3f818fde5a974ede2cd61d1bc4
Mark Petell <mark.petell@gmail.com>
2018-11-27 09:08:51 -0500

    Form upload changes

--- src/app/form-upload/form-upload.component.ts
+++ src/app/form-upload/form-upload.component.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormUploadComponent implements OnInit {
 
+  forms = [
+    {
+      formName: "New Patient Forms",
+      formDescription: "All the forms you will need to fill out before your first appointment with your doctor.",
+      formLink: "",
+    },
+    {
+      formName: "Health Information Release Authorization",
+      formDescription: "Fill this out if you would like us to release health information to a company or individual.",
+      formLink: "",
+    },
+    {
+      formName: "Health History Update Form",
+      formDescription: "Fill this out if there have been developments in yoru health since the last time we saw you.",
+      formLink: "",
+    },
+    {
+      formName: "Patient Information Update Form",
+      formDescription: "Fill this out if your contact or other personal information has changed.",
+      formLink: "",
+    },
+    {
+      formName: "Patient Feedback Form",
+      formDescription: "Let us know how we're doing.",
+      formLink: "",
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
